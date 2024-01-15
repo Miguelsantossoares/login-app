b7ff5b607a5605f26543f851997ee7002e27bed5
Miguel Santos <enium.miguel@gmail.com>
2024-01-15 11:07:35 -0300

    Finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,147 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="10">
+        <v-card class="elevation-6 mt-10">
+          <v-window v-model="step">
+            <v-window-item :value="1">
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <v-card-text class="mt-12">
+                    <h4 class="text-center">Login in to Your Account</h4>
+                    <h6 class="text-center grey--text">Login in to your account so you can continue building
+                      <br>and editing your onboarding flows
+                    </h6>
+                    <v-row align="center" justify="center">
+                      <v-col cols="12" sm="8">
+                        <v-text-field label="Email" outlined dense color="blue" autocomplete="false" class="mt-16"></v-text-field>
+                        <v-text-field label="Password" outlined dense color="blue" autocomplete="false" type="password"></v-text-field>
+                        <v-row>
+                          <v-col cols="12" sm="7">
+                            <v-checkbox label="Remember me" class="mt-n1" color="blue"></v-checkbox>
+                          </v-col>
+                          <v-col cols="12" sm="5">
+                            <span class="caption blue--text">Forget password</span>
+                          </v-col>
+                        </v-row>
+                        <v-btn color="blue" dark block tile>Log in</v-btn>
+                        <h5 class="text-center grey--text mt-4 mb-3">Or log in using</h5>
+                        <div class="d-flex justify-space-between align-center mx-10 mb-16">
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="red">fab fa-google</v-icon>
+                          </v-btn>
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="blue">fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="light-blue lighten-3">fab fa-twitter</v-icon>
+                          </v-btn>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-col>
+                <v-col cols="12" sm="6" class="blue rounded-bl-xl" >
+                  <div style="text-align:center; padding: 180px 0;">
+                    <v-card-text class="white--text">
+                      <h3 class="text-center">Don't have an Account Yet?</h3>
+                      <h6 class="text-center">
+                        Let's get you all set up so you can start creating your first<br>
+                        onboarding experience
+                      </h6>
+                    </v-card-text>
+                    <div class="text-center">
+                      <v-btn tile outlined dark @click="step++">SIGN UP</v-btn>
+                    </div>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-window-item>
+            <v-window-item :value="2">
+              <v-row>
+                <v-col cols="12" md="6" class="blue rounded-br-xl">
+                     <div style="  text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text" >
+                        <h3 class="text-center ">Alredy Signed up?</h3>
+                        <h6
+                          class="text-center"
+                        >Log in to your account so you can continue building and<br>  editing your onboarding flows</h6>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn tile outlined dark @click="step--">Log in</v-btn>
+                      </div>
+                      </div>
+                    </v-col>
+                <v-col cols="12" sm="6">
+                  <v-card-text class="mt-12">
+                    <h4 class="text-center">Sign up for an Account</h4>
+                    <h6 class="text-center grey--text">
+                      Let's get you all set up so you can start creation your <br>
+                      first onboarding experience
+                    </h6>
+                    <v-row align="center" justify="center">
+                      <v-col cols="12" sm="8">
+                        <v-row>
+                          <v-col class="12" sm="6">
+                            <v-text-field label="First Name" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
+                          </v-col>
+                          <v-col class="12" sm="6">
+                            <v-text-field label="Last Name" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
+                          </v-col>
+                        </v-row>
+                        <v-text-field label="Email" outlined dense color="blue" autocomplete="false"></v-text-field>
+                        <v-text-field label="Password" outlined dense color="blue" type="password"></v-text-field>
+                        <v-row>
+                          <v-col cols="12" sm="7">
+                            <v-checkbox label="I Accept AAE" class="mt-n1" color="blue"></v-checkbox>
+                          </v-col>
+                          <v-col cols="12" sm="5">
+                            <span class="caption blue--text ml-n4">Terms & Conditions</span>
+                          </v-col>
+                        </v-row>
+                        <v-btn color="blue" dark block tile>Sign up</v-btn>
+                        <h4 class="text-center grey-text mt-4 mb-3O">Or Sign up using</h4>
+                        <div class="d-flex justify-space-between align-center mx-10 mb-11">
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="red">fab fa-google</v-icon>
+                          </v-btn>
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="blue">fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn depressed outlined color="grey">
+                            <v-icon color="light-blue lighten-3">fab fa-twitter</v-icon>
+                          </v-btn>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'HomeView',
-
-    components: {
-      HelloWorld,
-    },
+    data: () => ({
+      step: 1
+    }),
+    props: {
+      source: String
+    }
+    
   }
 </script>
+<style scoped>
+  .v-application .rounded-bl-xl {
+    border-bottom-left-radius: 300px !important;
+  }
+  .v-application .rounded-br-xl {
+    border-bottom-right-radius: 300px !important;
+  }
+</style>
