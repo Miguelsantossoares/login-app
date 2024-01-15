d6f374639ba85b0007da1c3182aba07988dce2db
Miguel Santos <enium.miguel@gmail.com>
2024-01-15 11:16:54 -0300

    Traduzido

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,24 +8,24 @@
               <v-row>
                 <v-col cols="12" sm="6">
                   <v-card-text class="mt-12">
-                    <h4 class="text-center">Login in to Your Account</h4>
-                    <h6 class="text-center grey--text">Login in to your account so you can continue building
-                      <br>and editing your onboarding flows
+                    <h4 class="text-center">Faça login na sua conta</h4>
+                    <h6 class="text-center grey--text">Faça login em sua conta para poder continuar<br>
+                      e editar seus fluxos de integração
                     </h6>
                     <v-row align="center" justify="center">
                       <v-col cols="12" sm="8">
                         <v-text-field label="Email" outlined dense color="blue" autocomplete="false" class="mt-16"></v-text-field>
-                        <v-text-field label="Password" outlined dense color="blue" autocomplete="false" type="password"></v-text-field>
+                        <v-text-field label="Senha" outlined dense color="blue" autocomplete="false" type="password"></v-text-field>
                         <v-row>
                           <v-col cols="12" sm="7">
-                            <v-checkbox label="Remember me" class="mt-n1" color="blue"></v-checkbox>
+                            <v-checkbox label="Lembrar-me" class="mt-n1" color="blue"></v-checkbox>
                           </v-col>
                           <v-col cols="12" sm="5">
-                            <span class="caption blue--text">Forget password</span>
+                            <span class="caption blue--text">Esqueci a senha</span>
                           </v-col>
                         </v-row>
                         <v-btn color="blue" dark block tile>Log in</v-btn>
-                        <h5 class="text-center grey--text mt-4 mb-3">Or log in using</h5>
+                        <h5 class="text-center grey--text mt-4 mb-3">Ou faça login usando</h5>
                         <div class="d-flex justify-space-between align-center mx-10 mb-16">
                           <v-btn depressed outlined color="grey">
                             <v-icon color="red">fab fa-google</v-icon>
@@ -44,14 +44,14 @@
                 <v-col cols="12" sm="6" class="blue rounded-bl-xl" >
                   <div style="text-align:center; padding: 180px 0;">
                     <v-card-text class="white--text">
-                      <h3 class="text-center">Don't have an Account Yet?</h3>
+                      <h3 class="text-center">Ainda não tem uma conta?</h3>
                       <h6 class="text-center">
-                        Let's get you all set up so you can start creating your first<br>
-                        onboarding experience
+                        Vamos preparar tudo para que você possa começar a criar sua primeira<br>
+                        experiência de integração
                       </h6>
                     </v-card-text>
                     <div class="text-center">
-                      <v-btn tile outlined dark @click="step++">SIGN UP</v-btn>
+                      <v-btn tile outlined dark @click="step++">ASSINAR</v-btn>
                     </div>
                   </div>
                 </v-col>
@@ -62,45 +62,45 @@
                 <v-col cols="12" md="6" class="blue rounded-br-xl">
                      <div style="  text-align: center; padding: 180px 0;">
                       <v-card-text class="white--text" >
-                        <h3 class="text-center ">Alredy Signed up?</h3>
+                        <h3 class="text-center ">Já se registrou?</h3>
                         <h6
                           class="text-center"
-                        >Log in to your account so you can continue building and<br>  editing your onboarding flows</h6>
+                        >Faça login na sua conta para poder continuar criando e editando seus fluxos de integração</h6>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn tile outlined dark @click="step--">Log in</v-btn>
+                        <v-btn tile outlined dark @click="step--">Fazer login</v-btn>
                       </div>
                       </div>
                     </v-col>
                 <v-col cols="12" sm="6">
                   <v-card-text class="mt-12">
-                    <h4 class="text-center">Sign up for an Account</h4>
+                    <h4 class="text-center">Registrar-se em uma conta</h4>
                     <h6 class="text-center grey--text">
-                      Let's get you all set up so you can start creation your <br>
-                      first onboarding experience
+                      Vamos preparar tudo para que você possa começar a criar sua <br>
+                      primeira experiência de integração
                     </h6>
                     <v-row align="center" justify="center">
                       <v-col cols="12" sm="8">
                         <v-row>
                           <v-col class="12" sm="6">
-                            <v-text-field label="First Name" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
+                            <v-text-field label="Primeiro nome" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
                           </v-col>
                           <v-col class="12" sm="6">
-                            <v-text-field label="Last Name" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
+                            <v-text-field label="Sobrenome" outlined dense color="blue" autocomplete="false" class="mt-4"></v-text-field>
                           </v-col>
                         </v-row>
                         <v-text-field label="Email" outlined dense color="blue" autocomplete="false"></v-text-field>
-                        <v-text-field label="Password" outlined dense color="blue" type="password"></v-text-field>
+                        <v-text-field label="Senha" outlined dense color="blue" type="password"></v-text-field>
                         <v-row>
                           <v-col cols="12" sm="7">
-                            <v-checkbox label="I Accept AAE" class="mt-n1" color="blue"></v-checkbox>
+                            <v-checkbox label="Eu aceito AAE" class="mt-n1" color="blue"></v-checkbox>
                           </v-col>
                           <v-col cols="12" sm="5">
-                            <span class="caption blue--text ml-n4">Terms & Conditions</span>
+                            <span class="caption blue--text ml-n4">Termos e condições</span>
                           </v-col>
                         </v-row>
-                        <v-btn color="blue" dark block tile>Sign up</v-btn>
-                        <h4 class="text-center grey-text mt-4 mb-3O">Or Sign up using</h4>
+                        <v-btn color="blue" dark block tile>Registrar-se</v-btn>
+                        <h4 class="text-center grey-text mt-4 mb-3O">Ou registre-se usando</h4>
                         <div class="d-flex justify-space-between align-center mx-10 mb-11">
                           <v-btn depressed outlined color="grey">
                             <v-icon color="red">fab fa-google</v-icon>
